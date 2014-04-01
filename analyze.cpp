9ff4693e88284b75514b6178a8ddbf9e4bdef45f
Kerdab <irabraheem@gmail.com>
2014-04-01 18:41:58 -0500

    Now prints president names

--- analyze.cpp
+++ analyze.cpp
@@ -20,7 +20,8 @@ FILE* Fp;
 typedef struct speech_Num{
 	int wordCount, sentenceCount, lineCount, parCount;
     int iCount, iveCount, illCount, meCount, myCount, mineCount, myselfCount; 
-    int weCount, weveCount, wellCount, usCount, ourCount, oursCount, ourselvesCount;
+    int weCount, weveCount, wellCount, usCount, ourCount, oursCount; 
+    int ourselvesCount;
     float avgWordLength, avgSentLength;
 
 } SPEECH_NUM;
@@ -32,26 +33,66 @@ void printTableHeaders(){
 	printf("Program #4: Analysis\n");
 	printf("\n");
 
-  	printf("Speech\n");
-  	printf("\t\t\t\t\t\tAvg Word \t  Avg Sentence\n");
-  	printf("YEAR\tWords\tSentences\tParagraphs\t Length \t Length(in words)   \t");
-    printf("Singular Pronouns\t Plural Pronouns\tRatio Singular/Plural\n");
+  	printf("\t\t\t\t\t\t\t\tAvg Word \t  Avg Sentence\n");
+  	printf("  President Name   \tYEAR\tWords\tSentences   Paragraphs\t Length \t Length(in words)");
+    printf("\tSingular Pronouns\t Plural Pronouns\tRatio Singular/Plural\n");
 
-    printf("====\t-----\t---------\t----------\t--------\t-----------------");
+    printf("===================\t====\t-----\t---------   ----------\t--------\t-----------------");
     printf("\t-----------------\t-----------------\t---------------------\n");
 }
+
+
+//Names of Presidents
+void getPresidentName( char presidentName[], int i){
+
+const char* allNames[] = {"1.George Washington    ", "2.George Washington    ", 
+					"3.John Adams           ", "4.Thomas Jefferson     ",
+					"5.Thomas Jefferson     ", "6.James Madison        ", 
+					"7.James Madison        ", "8.James Monroe         ",
+					"9.James Monroe         ", "10.John Q Adams        ",
+					"11.Andrew Jackson      ", "12.Andrew Jackson      ",
+					"13.Martin Van Buren    ", "14.William H Harrison  ",
+					"15.James K Polk        ", "16.Zachary Taylor      ",
+					"17.Franklin Pierce     ", "18.James Buchanan      ",
+					"19.Abraham Lincoln     ", "20.Abraham Lincoln     ",
+					"21.Ulysses S Grant     ", "22.Ulysses S Grant     ",
+					"23.Rutherford B Hayes  ", "24.James A Garfield    ",
+					"25.Grover CLeveland    ", "26.Benjamin Harrison   ",
+					"27.Grover Cleveland    ", "28.William McKinley    ",
+					"29.William McKinley    ", "30.Theodore Roosevelt  ",
+					"31.William H Taft      ", "32.Woodrow Wilson      ",
+					"33.Woodrow Wilson      ", "34.Warren G Harding    ",
+					"35.Calvin Coolidge     ", "36.Herbert C Hoover    ",
+					"37.Franklin D Roosevelt", "38.Franklin D Roosevelt",
+					"39.Franklin D Roosevelt", "40.Franklin D Roosevelt",
+					"41.Harry S Truman      ", "42.Dwight D Eisenhower ",
+					"43.Dwight D Eisenhower ", "44.John F Kennedy      ",
+					"45.Lyndon B Johnson    ", "46.Richard M Nixon     ",
+					"47.Richard M Nixon     ", "48.Jimmy Carter        ",
+					"49.Ronald Reagan       ", "50.Ronald Reagan       ",
+					"51.George HW Bush      ", "52.Bill Clinton        ",
+					"53.Bill Clinton        ", "54.George W Bush       ",
+					"55.George W Bush       ", "56.Barack Obama        ",
+					"57.Barack Obama        " };
+
+		strcpy( presidentName, allNames[i]);
+}
+
 
 //Counts words, sentences, and paragraphs
 void countAndPrintInfo(SPEECH_NUM speechNum[], int i, const char* fileName){
 
-	char *speech = NULL, ch, *token;
-	int k = 0, size = 0;
-	float alphabetCount=0.0, avgWordLength;
-	char speechYear[4]; 
-	char filePath[25] = "./datafiles/"; 
-	float ratioSingToPlur;
-	int totalSing, totalPlur;
+  char *speech = NULL, ch, *token;
+  int k = 0, size = 0;
+  float alphabetCount=0.0, avgWordLength;
+  char presidentName[25]; //Name of president
+  char speechYear[4]; 
+  char filePath[25] = "./datafiles/"; 
+  float ratioSingToPlur;
+  int totalSing, totalPlur;
 
+  	//To get name of president
+  	getPresidentName( presidentName, i);
 	//To get the speech year
 	strncpy(speechYear, fileName, sizeof(speechYear)); 
 	//Merges fileName with filePath to give the complete file path
@@ -172,24 +213,28 @@ void countAndPrintInfo(SPEECH_NUM speechNum[], int i, const char* fileName){
 	//Takes care of the absence of the last space in the file
 	speechNum[i].wordCount++; 
 
-	//Prints all acquired info
-	printf("%s\t %4d\t      %3d\t\t%2d\t   %.2f \t \t   %.2f \t", speechYear,
-				speechNum[i].wordCount, speechNum[i].sentenceCount, 
-				speechNum[i].parCount, speechNum[i].avgWordLength, 
-				speechNum[i].avgSentLength);
-	
-
 	//Calculate ratio of singular pronouns to plural pronouns
-	totalSing = speechNum[i].iCount + speechNum[i].iveCount + speechNum[i].illCount +
-		speechNum[i].meCount + speechNum[i].myCount + speechNum[i].mineCount + speechNum[i].myselfCount;
+	totalSing = speechNum[i].iCount + speechNum[i].iveCount + 
+				speechNum[i].illCount + speechNum[i].meCount + 
+				speechNum[i].myCount + speechNum[i].mineCount + 
+				speechNum[i].myselfCount;
 
-	totalPlur = speechNum[i].weCount + speechNum[i].weveCount + speechNum[i].wellCount +
-			speechNum[i].usCount + speechNum[i].ourCount + speechNum[i].oursCount + 
-			speechNum[i].ourselvesCount;
+	totalPlur = speechNum[i].weCount + speechNum[i].weveCount + 
+				speechNum[i].wellCount + speechNum[i].usCount + 
+				speechNum[i].ourCount + speechNum[i].oursCount + 
+				speechNum[i].ourselvesCount;
 
 	ratioSingToPlur = (float)totalSing/(float)totalPlur;
 
-	printf("\t      %3d \t\t      %3d\t\t\t%3.2f\n", totalSing, totalPlur,ratioSingToPlur);
+	//Prints all acquired info
+	printf("%s%5s %7d %8d %11d %11.2f %20.2f \t", presidentName, 
+				speechYear,
+				speechNum[i].wordCount, speechNum[i].sentenceCount, 
+				speechNum[i].parCount, speechNum[i].avgWordLength, 
+				speechNum[i].avgSentLength);
+	//printf("\n");
+	printf("\t      %3d \t\t      %3d\t\t\t%3.2f\n", totalSing, totalPlur,
+				ratioSingToPlur);
 
 	free( speech); //Frees allocated memory
 }
