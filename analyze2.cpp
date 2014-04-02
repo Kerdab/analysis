32ff79457d8718e8a912af388c21dbcd7b6333f8
Kerdab <irabraheem@gmail.com>
2014-04-01 22:56:00 -0500

    added president names

--- analyze2.cpp
+++ analyze2.cpp
@@ -25,21 +25,58 @@ void printTableHeaders(){
 	printf("Program #4: Analysis\n");
     printf("\n");
     
-  	printf("Speech\n");
-    printf("YEAR   peace  war  economy  slave  slavery  ");
+    printf("President Name\t\tYEAR   peace  war  economy  slave  slavery  ");
     printf("women  families  business  jobs  government  unemployment  ");
     printf("taxes  work  future  leadership  \n");
 
-    printf("====   -----  ---  -------  -----  -------  ");
+    printf("==============\t\t====   -----  ---  -------  -----  -------  ");
     printf("-----  --------  --------  ----  ----------  ------------  ");
     printf("-----  ----  ------  ----------  \n");
 }
 
 
+//Names of Presidents
+void getPresidentName( char presidentName[], int i){
+
+const char* allNames[] = {"1.George Washington    ", "2.George Washington    ", 
+					"3.John Adams           ", "4.Thomas Jefferson     ",
+					"5.Thomas Jefferson     ", "6.James Madison        ", 
+					"7.James Madison        ", "8.James Monroe         ",
+					"9.James Monroe         ", "10.John Q Adams        ",
+					"11.Andrew Jackson      ", "12.Andrew Jackson      ",
+					"13.Martin Van Buren    ", "14.William H Harrison  ",
+					"15.James K Polk        ", "16.Zachary Taylor      ",
+					"17.Franklin Pierce     ", "18.James Buchanan      ",
+					"19.Abraham Lincoln     ", "20.Abraham Lincoln     ",
+					"21.Ulysses S Grant     ", "22.Ulysses S Grant     ",
+					"23.Rutherford B Hayes  ", "24.James A Garfield    ",
+					"25.Grover CLeveland    ", "26.Benjamin Harrison   ",
+					"27.Grover Cleveland    ", "28.William McKinley    ",
+					"29.William McKinley    ", "30.Theodore Roosevelt  ",
+					"31.William H Taft      ", "32.Woodrow Wilson      ",
+					"33.Woodrow Wilson      ", "34.Warren G Harding    ",
+					"35.Calvin Coolidge     ", "36.Herbert C Hoover    ",
+					"37.Franklin D Roosevelt", "38.Franklin D Roosevelt",
+					"39.Franklin D Roosevelt", "40.Franklin D Roosevelt",
+					"41.Harry S Truman      ", "42.Dwight D Eisenhower ",
+					"43.Dwight D Eisenhower ", "44.John F Kennedy      ",
+					"45.Lyndon B Johnson    ", "46.Richard M Nixon     ",
+					"47.Richard M Nixon     ", "48.Jimmy Carter        ",
+					"49.Ronald Reagan       ", "50.Ronald Reagan       ",
+					"51.George HW Bush      ", "52.Bill Clinton        ",
+					"53.Bill Clinton        ", "54.George W Bush       ",
+					"55.George W Bush       ", "56.Barack Obama        ",
+					"57.Barack Obama        " };
+
+		strcpy( presidentName, allNames[i]);
+}
+
+
 //Counts frequency of words
-void counter( const char* fileName){
+void counter( const char* fileName, int i){
 
   char *speech = NULL, ch, *token;
+  char presidentName[25]; //Name of president
   char speechYear[4]; 
   char filePath[25] = "./datafiles/"; 
   int k = 0, size = 0;
@@ -52,6 +89,8 @@ void counter( const char* fileName){
   int god=0, work=0, future=0;
   int leadership=0;
 
+	//To get name of president
+  	getPresidentName( presidentName, i);
   	//To get the speech year
   	strncpy(speechYear, fileName, sizeof(speechYear)); 
   	//Merges fileName with filePath to give the complete file path
@@ -92,47 +131,7 @@ void counter( const char* fileName){
 
 	//Counts words
 	while( token != NULL){
-		/*
-		if(strcmp( token, "freedom") == 0)
-			freedom++;
-		if(strcmp( token, "liberty") == 0)
-			liberty++;
-		if(strcmp( token, "america") == 0)
-			america++;
-		if(strcmp( token, "american") == 0)
-			american++;		
-		if(strcmp( token, "country") == 0)
-			country++;
-		if(strcmp( token, "nation") == 0)
-			nation++;
-		if(strcmp( token, "ally") == 0)
-			ally++;
-		if(strcmp( token, "allies") == 0)
-			allies++;
-		if(strcmp( token, "people") == 0)
-			people++;			
-		if(strcmp( token, "defend") == 0)
-			defend++;
-		if(strcmp( token, "interest") == 0)
-			interest++;
-		if(strcmp( token, "interests") == 0)
-			interests++;	
-		if(strcmp( token, "responsibility") == 0)
-			responsibility++;
-		if(strcmp( token, "commitment") == 0)
-			commitment++;
-		if(strcmp( token, "constitution") == 0)
-			constitution++;
-		if(strcmp( token, "equal") == 0)
-			equal++;
-		if(strcmp( token, "social") == 0)
-			social++;
-		if(strcmp( token, "progressive") == 0)
-			progressive++;
-		if(strcmp( token, "capitalism") == 0)
-			capitalism++;
-		*/
-
+		
 		if(strcmp( token, "slave") == 0)
 			slave++;
 		if(strcmp( token, "slavery") == 0)
@@ -168,17 +167,8 @@ void counter( const char* fileName){
 
 		token = strtok( NULL, " ,.");
 	}
-	/*
-	printf("%s\t%d\t%d\t%d\t%d\t   %d\t   %d", speechYear,
-		peace, freedom, liberty, america, american, country);
-	printf("\t   %d\t   %d\t %d\t %d\t  %d", 
-		nation, ally, allies, people, defend);
-	printf("\t   %d\t\t%d\t  %d\t    %d\t\t  %d",
-		interest, interests, commitment, government, responsibility);
-	printf("\t\t %d\t\t%d\t%d\t%d\n",
-		constitution, slave, slavery, equal);
-	*/
-	printf("%s%6d %5d %5d %7d %7d ", speechYear,
+
+	printf("%s %4s%6d %5d %5d %7d %7d ", presidentName, speechYear,
 		peace, war, economy, slave, slavery);
 	printf("%8d %8d %8d %8d %8d %11d ",
 		women, families, business, jobs, government, unemployment);
@@ -214,7 +204,7 @@ int main(){
 
     for(i=0; i<MAX_SPCH; i++){
 
-    	counter( fileName[i]);
+    	counter( fileName[i], i);
     }
 
    return 0;
